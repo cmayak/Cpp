fdeec89b1b68f555ac11fc83e54aa9d13ddbfacd
cmayak13 <cmayak13@yahoo.com>
2019-07-20 13:41:14 -0400

    notes

---
+++ Loops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i = 1; i < 11; i++){
+        cout << i << endl; //counts up to 10
+    }
+
+    for(int j = 10; j = 0; j--){
+        cout << j << endl; //counts down from 10
+    }
+    
+    int x = 10;
+    while(--x){ // counts numbers down
+        cout << x << endl;
+    }
+}
